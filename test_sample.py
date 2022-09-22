f665cb90e7e4550dd0433770ea1976081a76cf4b
enveryasar <enver.yasar@bynder.com>
2022-09-22 15:19:17 +0200

    junit

--- test_sample.py
+++ test_sample.py
@@ -1,6 +1,17 @@
 import requests
 
-def test_http():
+def test_get_token():
+    url = "https://integrations.bynder-stage.com/v6/authentication/oauth2/token"
+
+    payload = 'client_id=64368a3f-963e-45c0-b9d9-f8bac02a8815&client_secret=abf25d20-6243-4d40-ac98-a1b46421ad3f&grant_type=client_credentials'
+    headers = {
+        'Content-Type': 'application/x-www-form-urlencoded'
+    }
+    response = requests.request("POST", url, headers=headers, data=payload)
+    print(response.status_code)
+    print(response.text)
+
+def test_get_a_new_token():
     url = "https://integrations.bynder-stage.com/v6/authentication/oauth2/token"
 
     payload = 'client_id=64368a3f-963e-45c0-b9d9-f8bac02a8815&client_secret=abf25d20-6243-4d40-ac98-a1b46421ad3f&grant_type=client_credentials'
