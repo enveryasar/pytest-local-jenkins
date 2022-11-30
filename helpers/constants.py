a20c7a62fffe73dc3c6061a39b057ff0c444532d
enveryasar <enver.yasar@bynder.com>
2022-11-30 15:38:39 +0100

    add movie api tests

--- helpers/constants.py
+++ helpers/constants.py
@@ -1,5 +1,7 @@
 import os
 
+BASE_URL = 'https://api.themoviedb.org/3'
+
 API_KEY = os.getenv(
     "API_KEY", ""
 )
--- tests/test_sample.py
+++ tests/test_sample.py
@@ -1,12 +1,47 @@
 import requests
-from helpers.constants import API_KEY
+import json
+from helpers.constants import API_KEY, BASE_URL
+import random
+obj = {}
 
-BASE_URL = 'https://api.themoviedb.org/3'
+randomPage = random.randint(10,400)
 
 
-def test_movie():
+def test_GET_top_rated_movies():
     params = {'api_key': API_KEY}
     response = requests.get(BASE_URL + '/movie/top_rated', params=params)
     assert response.status_code == 200
     print(response.status_code)
     print(response.text)
+
+def test_GET_incorrect_api_key_401():
+    params = {'api_key': 'wrong_key'}
+    response = requests.get(BASE_URL + '/movie/top_rated', params=params)
+    assert response.status_code == 401
+
+def test_GET_Should_not_top_rated_movies_with_page_0():
+    params = {
+        'api_key': API_KEY,
+        'page': 0
+        }
+    response = requests.get(BASE_URL + '/movie/top_rated', params=params)
+    assert response.status_code == 422
+
+def test_GET_Should_top_rated_movies_with_max_page():
+    params = {
+        'api_key': API_KEY,
+        'page': 500
+        }
+    response = requests.get(BASE_URL + '/movie/top_rated', params=params)
+    assert response.status_code == 200
+
+def test_GET_Should_top_rated_movies_with_random_page_between_1_500():
+    params = {
+        'api_key': API_KEY,
+        'page': randomPage
+        }
+    response = requests.get(BASE_URL + '/movie/top_rated', params=params)
+    assert response.status_code == 200
+    data = json.loads(response.content)
+    assert data["page"] == randomPage
+    obj["movieId"] = data["results"][0]["id"]
